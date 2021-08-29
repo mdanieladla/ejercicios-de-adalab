11c2cb7703c05f7d3d3bce3a0c1cd9defb3ce0db
mdanieladla <mariadanieladarnea@gmail.com>
2021-08-29 15:54:46 +0200

    add comment duda

--- modulo-2/modulo-2-leccion-04/ejercicio-10/main.js
+++ modulo-2/modulo-2-leccion-04/ejercicio-10/main.js
@@ -1,5 +1,15 @@
 'use strict'
 
-function border(border, width, padding, size) {
-
+function border(boxSizing, width, padding, borderSize) {
+    if (boxSizing === true) {
+        //console.log('pasa x aqui')
+        console.log(`Como es border-box su ancho es ${width} y el ancho total es ${width + padding + borderSize}`);
+    } else {
+        console.log(`Como es content-box su ancho es ${width} y el ancho total es ${width + padding}`);
+    }
+    
 }
+
+border(false,15,5,12);
+
+//DUDA: porque si pongo let o const size me sale error y no funciona, pero con var si funciona. 
