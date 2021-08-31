8bc22dfa22178f43ce9547703608bc48ecaa39a2
mdanieladla <mariadanieladarnea@gmail.com>
2021-08-31 19:41:07 +0200

    complete first part of the exercise

--- modulo-2/repaso intermedio/js/main.js
+++ modulo-2/repaso intermedio/js/main.js
@@ -1,1 +1,27 @@
 'use strict'
+
+const button = document.querySelector('.js-btn');
+
+const body = document.querySelector('.container');
+
+const emoji = document.querySelector('.js-emoji');
+
+function randomNumber () {
+    const randomNumber = Math.round(Math.random() * 100);
+    return randomNumber;
+}
+
+function paintBackground(){
+  const par = randomNumber() % 2;   
+    if (par === 0) {
+        body.classList.add('containerHappy');
+        body.classList.remove('containerSad');
+    } else {
+        body.classList.add('containerSad');
+        body.classList.remove('containerHappy');
+    }
+}
+
+button.addEventListener('click', paintBackground);
+
+//Falta segunda parte.
