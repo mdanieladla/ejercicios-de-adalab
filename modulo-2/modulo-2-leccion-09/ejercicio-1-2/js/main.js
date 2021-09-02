c62604e150487a70a63e403651383189e8ff9718
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-02 11:18:00 +0200

    add console.log to first function and comment to second

--- modulo-2/modulo-2-leccion-09/ejercicio-1-2/js/main.js
+++ modulo-2/modulo-2-leccion-09/ejercicio-1-2/js/main.js
@@ -7,8 +7,8 @@ let arr = [];
 function get100Numbers(){
     for(let i = 1; i <=100; i++) {
         arr.push(i);
+        console.log(i)
     }
-    return arr;
 }
 //*//
 
@@ -17,5 +17,6 @@ function get100Numbers(){
 function getReversed100Numbers () {
     arr.reverse();
     return arr;
+    //hacer un for para llamar
 }
 //*//
