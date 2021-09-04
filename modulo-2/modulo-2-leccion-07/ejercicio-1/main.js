d847b8a9de0152e79e180370a692190d2b69050e
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-04 17:33:37 +0200

    add exercise

--- modulo-2/modulo-2-leccion-07/ejercicio-1/main.js
+++ modulo-2/modulo-2-leccion-07/ejercicio-1/main.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const movies = ['Interstellar', 'The arrival', 'Civil War', 'Coherence'];
+
+function workWithMovies(){
+    return movies;
+}
+
+workWithMovies();
+
+//console.log(movies);
