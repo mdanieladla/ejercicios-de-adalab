5fcdb84cc6db5353850be9e2d08e125dff76c360
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-16 21:40:09 +0200

    add header of evaluacio final modulo 1 to React with SASS

---
+++ modulo-3/modulo-3-leccion-01-1.8/src/components/App.js
@@ -0,0 +1,41 @@
+import '../styles/App.scss';
+import cover from '../images/cover.jpg';
+
+function App() {
+  return (
+    <div>
+      <header className='header' styles={{ backgroundImage: `url(${cover})` }}>
+        <nav>
+          <a
+            href='https://adalab.es/'
+            rel='noreferrer'
+            target='_blank'
+            className='header__icomenu'
+          >
+            <img
+              src='./assets/images/ico-menu.svg'
+              alt='menú'
+              title='Go to the page of Adalab'
+            />
+          </a>
+        </nav>
+        <div className='header__container' id='top'>
+          <h1 className='header__container--title'>Anonymous proxy</h1>
+          <h2 className='header__container--paragraph'>
+            In hac habitasse platea dictumst.
+          </h2>
+          <a href='#purchase'>
+            <img
+              src='./assets/images/ico-scroll-down.svg'
+              alt='scroll down icon'
+              className='button__scroll-down'
+              title='Go to 3 reasons to purchase'
+            />
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
