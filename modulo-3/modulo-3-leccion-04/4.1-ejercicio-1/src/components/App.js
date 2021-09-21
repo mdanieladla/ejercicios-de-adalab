a4a9ff6e78079249c0507ff94f8e0f2f7c5a1689
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-21 18:41:16 +0200

    add exercise 2: array de objetos and commented exercise 1

--- modulo-3/modulo-3-leccion-04/4.1-ejercicio-1/src/components/App.js
+++ modulo-3/modulo-3-leccion-04/4.1-ejercicio-1/src/components/App.js
@@ -1,18 +1,47 @@
 import '../styles/App.scss';
 
 function App() {
+  //Ejercicio 1: array de strings
+  // const tasks = [
+  //   'Comprar harina, jamón y pan rallado',
+  //   'Hacer croquetas ricas',
+  //   'Ir a la puerta de un gimnasio',
+  //   'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+  // ];
+
+  // const renderTasks = () => {
+  //   return tasks.map((task) => {
+  //     return <li>{task}</li>;
+  //   });
+  // };
+
+  /* *** */
+
+  //Ejercicio 2: array de objetos
   const tasks = [
-    'Comprar harina, jamón y pan rallado',
-    'Hacer croquetas ricas',
-    'Ir a la puerta de un gimnasio',
-    'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    {
+      task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+      completed: false,
+    },
   ];
 
   const renderTasks = () => {
-    return tasks.map((task) => {
-      return <li>{task}</li>;
+    return tasks.map((eachTask) => {
+      if (eachTask.completed === true) {
+        return (
+          <li key={eachTask.task} className='completed'>
+            {eachTask.task}
+          </li>
+        );
+      } else {
+        return <li key={eachTask.task}>{eachTask.task}</li>;
+      }
     });
   };
+
   return (
     <div>
       <h1>Hola mundo</h1>
