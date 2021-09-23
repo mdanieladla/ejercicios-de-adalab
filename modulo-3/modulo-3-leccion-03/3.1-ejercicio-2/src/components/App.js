517d862b94a1dd5d4060a8a33428bf71f93e50cc
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-23 20:13:33 +0200

    add finished exercise

--- modulo-3/modulo-3-leccion-03/3.1-ejercicio-2/src/components/App.js
+++ modulo-3/modulo-3-leccion-03/3.1-ejercicio-2/src/components/App.js
@@ -1,13 +1,22 @@
 import '../styles/App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
   const handleWrite = (ev) => {
-    return ev.target.value;
+    const text = ev.currentTarget.value;
+    return setText(text.replaceAll(/[aeiou]/gi, 'i'));
   };
   return (
     <div>
-      <p onKeyUp={handleWrite}></p>
-      <textarea name='mimimi' cols='30' rows='10'></textarea>
+      <p>{text}</p>
+      <textarea
+        name='mimimi'
+        cols='30'
+        rows='10'
+        onKeyUp={handleWrite}
+      ></textarea>
     </div>
   );
 }
