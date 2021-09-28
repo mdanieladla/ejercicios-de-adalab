8ba1d504df49dace41b45efa6c43fcb19f296c64
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-28 19:27:19 +0200

    add router

--- react-starter-kit-template/src/components/App.js
+++ react-starter-kit-template/src/components/App.js
@@ -1,11 +1,31 @@
-import '../styles/App.scss';
+// Fichero src/components/App.js
 
-function App() {
+import { Route } from 'react-router-dom';
+
+const App = () => {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h2>Este título aparece siempre</h2>
+
+      <Route path='/contacto'>
+        <h2>
+          Este título solo aparece cuando la usuaria entra en la página de
+          contacto
+        </h2>
+      </Route>
+
+      <nav>
+        <ul>
+          <li>
+            <a href='/#/'>Ir al inicio</a>
+          </li>
+          <li>
+            <a href='/#/contacto'>Ir a contacto</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
 
 export default App;
